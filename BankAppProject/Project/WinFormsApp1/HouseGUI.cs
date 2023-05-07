27df6ba6e86309666c9039c911ec43b80a899619
sskowalik <130567074+sskowalik@users.noreply.github.com>
2023-05-07 18:07:15 +0200

    Add files via upload

---
+++ BankAppProject/Project/WinFormsApp1/HouseGUI.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace WinFormsApp1
+{
+    public partial class HouseGUI : Form
+    {
+        public HouseGUI()
+        {
+            InitializeComponent();
+        }
+        House house= new House();
+
+        public House returnHouse()
+        {
+            house.price= Convert.ToInt32(textBox1.Text);
+            house.buildingPlotWidth = Convert.ToInt32(textBox2.Text);
+            house.buildingPlotLength = Convert.ToInt32(textBox3.Text);
+            house.BuildingPermitNumber = textBox4.Text;
+
+            return house;
+        }
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            returnHouse();
+            if (textBox1.Text != "" && textBox2.Text !="" && textBox3.Text != "" && textBox4.Text != "")
+            {
+                var x = MessageBox.Show("House parameters:\nPrice: " + house.price + " " + +house.buildingPlotWidth + " x " + house.buildingPlotLength + "\n" + house.BuildingPermitNumber);
+                Close();
+            }
+            else { var x = MessageBox.Show("You have to fill all data!"); }
+        }
+    }
+}
---
+++ BankAppProject/Project/WinFormsApp1/Mortgage.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinFormsApp1
+{
+    internal class Mortgage : Loan
+    {
+
+        User User = new User();
+        Money Money = new Money();
+        public Mortgage() { }
+        public Mortgage(User user, Money money)
+        {
+            User = user;
+            Money = money;
+        }
+        public void TakeLoan(Money money, int amount)
+        {
+            money.changeAmountOfMoney(amount);
+        }
+
+    }
+}
